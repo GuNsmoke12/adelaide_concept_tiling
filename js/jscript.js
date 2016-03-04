e16abcf6228fa979efecc62a0e059b62925d8d0f
GunSmoke12 <me_most-wanted_12@hotmail.com>
2016-03-04 18:29:55 +0000

    Further building been done.

--- js/jscript.js
+++ js/jscript.js
@@ -1,5 +1,80 @@
 $(document).ready(function() {
  
-  $("#owl-example").owlCarousel();
+    $("#owl-carousel").owlCarousel({
+
+            // Most important owl features
+        items : 5,
+        itemsCustom : false,
+        itemsDesktop : [1199,4],
+        itemsDesktopSmall : [980,3],
+        itemsTablet: [768,2],
+        itemsTabletSmall: false,
+        itemsMobile : [479,1],
+        singleItem : true,
+        itemsScaleUp : true,
+
+        //Basic Speeds
+        slideSpeed : 200,
+        paginationSpeed : 800,
+        rewindSpeed : 1000,
+
+        //Autoplay
+        autoPlay : true,
+        stopOnHover : false,
+
+        // Navigation
+        navigation : false,
+        navigationText : ["prev","next"],
+        rewindNav : true,
+        scrollPerPage : false,
+
+        //Pagination
+        pagination : false,
+        paginationNumbers: false,
+
+        // Responsive 
+        responsive: true,
+        responsiveRefreshRate : 200,
+        responsiveBaseWidth: window,
+
+        // CSS Styles
+        baseClass : "owl-carousel",
+        theme : "owl-theme",
+
+        //Lazy load
+        lazyLoad : false,
+        lazyFollow : true,
+        lazyEffect : "fade",
+
+        //Auto height
+        autoHeight : false,
+
+        //JSON 
+        jsonPath : false, 
+        jsonSuccess : false,
+
+        //Mouse Events
+        dragBeforeAnimFinish : true,
+        mouseDrag : false,
+        touchDrag : false,
+
+        //Transitions
+        transitionStyle : "fade",
+
+        // Other
+        addClassActive : false,
+
+        //Callbacks
+        beforeUpdate : false,
+        afterUpdate : false,
+        beforeInit: false, 
+        afterInit: false, 
+        beforeMove: false, 
+        afterMove: false,
+        afterAction: false,
+        startDragging : false,
+        afterLazyLoad : false
+
+    })
  
 });
