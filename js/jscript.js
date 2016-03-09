d8b9723b4f4139887968c2555da9f5fb48be3a40
GunSmoke12 <me_most-wanted_12@hotmail.com>
2016-03-09 23:32:26 +0000

    Started implementing facebook feed. Currently using JQuery to grab JSON file and manipulate object values.

--- js/jscript.js
+++ js/jscript.js
@@ -98,9 +98,79 @@
         })
     }
     
+    
+    var loadFeed = function() {
+        var postsData, imagesData, profileImage, id, i, x, y, imagesArray,
+            postsUrl = 'https://graph.facebook.com/v2.5/adelaideconcepttiling/posts?access_token=1703185049966389%7CnLQl9at6iYAmMzH85XQINKXbf8g',
+            imagesUrl = 'https://graph.facebook.com/v2.5/adelaideconcepttiling/posts?fields=attachments%7Bsubattachments%7Bmedia%7Bimage%7Bsrc%7D%7D%7D%7D&access_token=1703185049966389%7CnLQl9at6iYAmMzH85XQINKXbf8g',
+            profilePicUrl = "https://graph.facebook.com/v2.5/adelaideconcepttiling?fields=picture&access_token=1703185049966389%7CnLQl9at6iYAmMzH85XQINKXbf8g";
+
+        $.when(
+            $.getJSON(postsUrl, function(data) {
+                postsData = data;
+            }),
+            $.getJSON(imagesUrl, function(data) {
+                imagesData = data;
+            }),
+            $.getJSON(profilePicUrl, function(data) {
+                profileImage = data.picture.data.url;
+            })
+        ).then(function() {
+            if (postsData && imagesData) {
+                console.log('json loaded');
+                for (i=0; i < postsData.data.length; i++) {
+                    id = postsData.data[i].id;
+                    if (!postsData.data[i].message) {
+                        postsData.data[i].message = postsData.data[i].story;
+                    }
+                    
+                    for (x=0; x < imagesData.data.length; x++) {
+                        if ((id === imagesData.data[x].id) && imagesData.data[x].attachments) {
+                            postsData.data[i].image = imagesData.data[x].attachments.data[0].subattachments.data[0].media.image.src;
+                            
+                        }
+                    }
+                }
+            }
+            console.log(postsData);
+            printFeed(postsData, profileImage);
+        });
+    }
+    
+    var printFeed = function (postsData, profileImage) {
+        var i, image, message, header, profilePic,
+            feed = $('.feed');
+        
+        for (i=0; i < postsData.data.length; i++) {
+            if (postsData.data[i].image) {
+                image = '<div class="post_image grid-parent grid-40 tablet-grid-50"><img src="' + postsData.data[i].image + '"></div>';
+            } else {
+                image = '';
+            }
+            
+            profilePic = '<img class="profile_pic" src="' + profileImage + '">';
+            header = '<h6>Adelaide Concept Tiling</h6>';
+            message = '<p>' + postsData.data[i].message + '</p>';
+            
+            feed.append('<div class="post">' +
+                            image +
+                            '<div class="post_message">' +
+                                profilePic +
+                                header +
+                                message +
+                            '</div>' +
+                        '</div>'
+            )
+            
+            $('.post').has('img').find('.post_message').addClass('grid-parent grid-60 tablet-grid-50');
+        }
+    }
+    
     $(document).ready(function() {
         initShowcaseSlide();
 
         initOwlCarousel();
+        
+        loadFeed();
     });
 }());
