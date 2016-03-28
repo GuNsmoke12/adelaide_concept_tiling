a0e9763aafb594d1f4423b8d880e106fa7f1d2b4
GunnyWelsh <GunnyWelsh>
2016-03-28 19:05:22 +0100

    Created facebook feed. Restyled and redisgned it. Removed some of the unneccessary FB data (eg. profile pic). Added Masonry library to get tiled grid. Added imageloaded library to fix masonry grid layout on page load. Recoded a lot of the facebook js I had originally written.

--- js/jscript.js
+++ js/jscript.js
@@ -50,7 +50,7 @@ function initOwlCarousel () {
         pagination : false,
         paginationNumbers: false,
 
-        // Responsive 
+        // Responsive
         responsive: true,
         responsiveRefreshRate : 200,
         responsiveBaseWidth: window,
@@ -67,8 +67,8 @@ function initOwlCarousel () {
         //Auto height
         autoHeight : false,
 
-        //JSON 
-        jsonPath : false, 
+        //JSON
+        jsonPath : false,
         jsonSuccess : false,
 
         //Mouse Events
@@ -85,9 +85,9 @@ function initOwlCarousel () {
         //Callbacks
         beforeUpdate : false,
         afterUpdate : false,
-        beforeInit: false, 
-        afterInit: false, 
-        beforeMove: false, 
+        beforeInit: false,
+        afterInit: false,
+        beforeMove: false,
         afterMove: false,
         afterAction: false,
         startDragging : false,
@@ -98,10 +98,10 @@ function initOwlCarousel () {
 
 
 function loadFeed () {
-    var imagesData, profileImage, id, i, x, y, imagesArray,
+    var imagesData, likesData, id, i, x, y, imagesArray,
         postsUrl = 'https://graph.facebook.com/v2.5/adelaideconcepttiling/posts?access_token=1703185049966389%7CnLQl9at6iYAmMzH85XQINKXbf8g',
         imagesUrl = 'https://graph.facebook.com/v2.5/adelaideconcepttiling/posts?fields=attachments%7Bsubattachments%7Bmedia%7Bimage%7Bsrc%7D%7D%7D%7D&access_token=1703185049966389%7CnLQl9at6iYAmMzH85XQINKXbf8g',
-        profilePicUrl = "https://graph.facebook.com/v2.5/adelaideconcepttiling?fields=picture&access_token=1703185049966389%7CnLQl9at6iYAmMzH85XQINKXbf8g";
+        likesCommentsUrl = 'https://graph.facebook.com/v2.5/adelaideconcepttiling/posts?fields=link%2Ccomments.limit(1).summary(true)%2Clikes.limit(1).summary(true)&access_token=1703185049966389%7CnLQl9at6iYAmMzH85XQINKXbf8g'
 
     $.when(
         $.getJSON(postsUrl, function(data) {
@@ -110,8 +110,8 @@ function loadFeed () {
         $.getJSON(imagesUrl, function(data) {
             imagesData = data.data;
         }),
-        $.getJSON(profilePicUrl, function(data) {
-            profileImage = data.picture.data.url;
+        $.getJSON(likesCommentsUrl, function(data) {
+            extraDetailsData = data.data;
         })
     ).then(function() {
         for (i=0; i < postsData.length; i++) {
@@ -123,42 +123,75 @@ function loadFeed () {
             for (x=0; x < imagesData.length; x++) {
                 if ((id === imagesData[x].id) && imagesData[x].attachments) {
                     postsData[i].image = imagesData[x].attachments.data[0].subattachments.data[0].media.image.src;
-
                 }
+            }
+
+            for (x=0; x < extraDetailsData.length; x++) {
+              if (id === extraDetailsData[x].id) {
+                  postsData[i].likes = extraDetailsData[x].likes.summary.total_count;
+                  postsData[i].comments = extraDetailsData[x].comments.summary.total_count;
+                  postsData[i].link = extraDetailsData[x].link || ('https://www.facebook.com/' + postsData[i].id);
+              }
             }
         }
 
-        printFeed(postsData, profileImage);
+        printFeed(postsData);
     });
 }
 
-function printFeed (postsData, profileImage) {
-    var i, image, message, header, profilePic,
-        feed = $('.feed');
+function printFeed (postsData) {
+    var i, message, comments, likes, createdTime, link, hideOnSmallScreen,
+        feed = $('.feed'),
+        postsShowLimit = 20;
+        mobileSizeShowLimit = 9;
 
-    for (i=0; i < postsData.length; i++) {
+    for (i=0; i < postsShowLimit; i++) {
         if (postsData[i].image) {
-            image = '<div class="post_image grid-parent grid-40 tablet-grid-50"><img src="' + postsData[i].image + '"></div>';
+            image = '<img src="' + postsData[i].image + '" />';
         } else {
             image = '';
         }
 
-        profilePic = '<img class="profile_pic" src="' + profileImage + '">';
-        header = '<h6>Adelaide Concept Tiling</h6>';
+        hideOnSmallScreen = i > mobileSizeShowLimit ? 'hideOnSmallScreen':'';
         message = '<p>' + postsData[i].message + '</p>';
+        createdTime = '<span class="post_date">' + new Date(postsData[i].created_time).toDateString() + '</span>';
+        comments = '<span class="post_count"><i class="fa fa-comments"></i>' + postsData[i].comments + '</span>';
+        likes = '<span class="post_count"><i class="fa fa-thumbs-up"></i>' + postsData[i].likes + '</span>';
+        link = '<a class="post_link" href="'+ postsData[i].link +'" target="_blank">view on Facebook <i class="fa fa-facebook"></i></a>'
 
-        feed.append('<div class="post">' +
-                        image +
-                        '<div class="post_message">' +
-                            profilePic +
-                            header +
-                            message +
-                        '</div>' +
-                    '</div>'
-        )
-
-        $('.post').has('img').find('.post_message').addClass('grid-parent grid-60 tablet-grid-50');
+        feed.append(
+          '<div class="post_wrapper grid-item ' + hideOnSmallScreen + '">' +
+              '<div class="post_image">' +
+                  image +
+              '</div>' +
+              '<div class="post_message">' +
+                  message +
+                  createdTime +
+              '</div>' +
+              '<div class="post_footer">' +
+                  comments +
+                  likes +
+                  link +
+              '</div>' +
+          '</div>'
+        );
     }
+
+    initMasonryGrid();
+}
+
+function initMasonryGrid() {
+    // init Masonry
+    var $grid = $('.grid').masonry({
+      columnWidth: '.grid-sizer',
+      gutter: '.gutter-sizer',
+      itemSelector: '.grid-item',
+      percentPosition: true
+    });
+    // layout Masonry after each image loads
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry('layout');
+    });
 }
 
 $(document).ready(function() {
